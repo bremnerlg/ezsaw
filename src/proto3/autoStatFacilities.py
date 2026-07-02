fe021a69ec47f6b4b3b0b26bd3194d24590fc953
bremnerlg <bremnerlgprogramming@gmail.com>
2026-07-02 08:19:37 -0400

    Removed files no longer needed, worked on structuring the SQL queries correctly.

--- src/proto3/autoStatFacilities.py
+++ src/proto3/autoStatFacilities.py
@@ -9,22 +9,8 @@ def prompt_window(message: str)
 def in_tolerance(vin: str):
 '''
 
-
-
-class test_case:
-    def __init__(self, step_name: str, y: float, 
-    lower: float, upper: float, v: str):
-       self.name = step_name
-       self.result = y
-       self.lower_limit = lower
-       self.upper_limit = upper
-       self.vin = v
-       if self.result < self.lower_limit or self.result > self.upper_limit:
-            self.out_of_tolerance = True
-       else:
-            self.out_of_tolerance = False 
-
-
+AUTO_STAT_FIELDS = ('auto_door_stat_name', 'result_x', 'result_x_unit', 'result_y_lower_lim',
+                    'result_y', 'result_y_upper_lim', 'result_y_unit', 'vin')
 
 def pull_decimal(sql_entry: str) -> float | None: # convert the sql output string to float group
     match = re.search(r'?*\d+\.\d+', sql_entry)
@@ -32,17 +18,45 @@ def pull_decimal(sql_entry: str) -> float | None: # convert the sql output strin
         return float(match.group())
     return None
 
-def vin_fetch(vin: str, fields: list, table: str) -> tuple:
-    conn = psycopg.connect(
-        dbname="ezsaw_proto",
+
+def vin_fetch_outliers(vin: str, fields: tuple, table1: str, table2: str) -> list:
+    conn = psycopg.connect( # eventually the JSON will be where the db server config is pulled from
+        dbname="ezsaw3",
         user="postgres",
         password="postgres",
         host="localhost"
     )
     cur = conn.cursor()
     # painful reminder that for some reason I decided to put lower before upper
-    cur.execute ('SELECT ' + ", ".join(fields) + ' FROM ' + table)
+    cur.execute ('SELECT ' + ", ".join(fields) + ' FROM ' + table1 + ' JOIN ' + table2 + ' ON ' + table1 + '.auto_door_stat_id' + ' = ' + 
+                table2 + '.auto_door_stat' + ' WHERE ' + table2 + '.vin = \'' + vin + '\';') 
+
     return cur.fetchall()  # return of a tuple of the VINs occurences in the steps table
+
+class test_case:
+    def __init__(self, test_name, x, x_unit, 
+                y_low, y, y_high, y_unit, vin):
+       self.name = test_name 
+       self.result_x = pull_decimal(x)
+       self.result_x_unit = unit_x 
+       self.result_y_lower = pull_decimal(y_low)
+       self.result_y = pull_decimal(y)
+       self.result_y_upper = pull_decimal(y_high)
+       self.result_y_unit = y_unit
+       self.vehicle = vin
+
+       if self.result_y < self.result_y_lower or self.result_y > self.result_y_upper:
+            self.out_of_tolerance = True
+       else:
+            self.out_of_tolerance = False 
+
+'''
+init_test_case():
+Wrapper function to make the test case instances cleaner
+Strongly depends on the AUTO_STAT_FIELDS to remain the same.
+'''
+def init_test_case(st: tuple) -> test_case:
+    return test_case(st[0], st[1], st[2], st[3], st[4], st[5], st[6], st[7])
 
 def results_dump(results):
     i_cnt = 0
@@ -68,8 +82,10 @@ def results_dump(results):
                   "upper_lim: " + str(test_results.upper_limit) + "\n" 
                   "lower_lim: " + str(test_results.lower_limit) + "\n")
 
-
+'''
 def main():
-    std_fields = ['step_name', 'y', 'lower_lim', 'upper_lim', 'vin']
+    std_fields = ['auto_door_stat_name', 'result_x', 'result_x_unit', 'result_y_lower_lim', 'result_y', 
+        'result_y_upper_lim', 'result_y_unit' ]
     results_dump(vin_fetch('1VWBT7A36EC345678', std_fields, 'steps'))
 main()
+'''
---
+++ src/proto3/main.py
@@ -0,0 +1,55 @@
+'''
+EZSAW V3.1.0A PYQT Edition
+ABANDONING KIVY.
+'''
+import sys
+import pyqtgraph as pg
+from PySide6.QtWidgets import (QApplication, QDialog, QLineEdit, 
+    QPushButton, QVBoxLayout)
+from autoStatFacilities import vin_fetch_outliers, init_test_case, AUTO_STAT_FIELDS
+
+
+class intro_form(QDialog):
+
+    def __init__(self, parent=None):
+        super(intro_form, self).__init__(parent)
+        self.setWindowTitle("EZSAW Version 3.1.0 Alpha")
+
+        self.edit = QLineEdit("Enter a VIN...")
+        self.button = QPushButton("Enter")
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.edit)
+        layout.addWidget(self.button)
+
+        self.setLayout(layout)
+
+        self.button.clicked.connect(self.display_entry_table)
+
+
+    def display_entry_table(self, vin: str):
+        outliers = vin_fetch_outliers(self.edit.text(), AUTO_STAT_FIELDS, 'auto_door_stats', 'steps')
+        print("\n\n---------TEST_RESULTS----------")
+        for i in outliers:
+            test_results = init_test_case(i)
+            i_cnt = i_cnt + 1
+
+            print(str(i[0]) + ': FAILED.')
+            print("Set: " + str(i_cnt))
+            print("VIN: " + str(test_results.vin) + "\n"
+                  "y val: " + str(test_results.result) + "\n" 
+                  "upper_lim: " + str(test_results.upper_limit) + "\n" 
+                  "lower_lim: " + str(test_results.lower_limit) + "\n")
+
+    def dbg_print_vin_fetch(self):
+        print(vin_fetch_outliers(self.edit.text(), AUTO_STAT_FIELDS, 'auto_door_stats', 'steps'))
+
+    
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    form = intro_form()
+    form.show()
+
+    sys.exit(app.exec())
